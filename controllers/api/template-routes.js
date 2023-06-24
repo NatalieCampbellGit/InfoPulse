52492a898887a466f5209f582e45619d20ebb13d
stanno03 <danno_41@hotmail.com>
2023-06-24 22:00:45 +1000

    added route for templates

---
+++ controllers/api/template-routes.js
@@ -0,0 +1,44 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-expressions */
+const sequelize = require('sequelize')
+const express = require('express')
+const Template = require('../../models/Template')
+const FactSheet = require('../../models/Factsheet')
+const UserComment = require('../../models/UserComment')
+const User = require('../../models/User')
+const Administrator = require('../../models/Administrator')
+const withAuth = require('../../utils/auth')
+const { template } = require('handlebars')
+const router = require('express').Router();
+
+router.get('/search', (res, req) => {
+    
+        const searchText = req.query.search;
+        const searchTemplateID = req.query.type;
+
+        let query = {};
+
+        if (searchTemplateID === 'id') {
+          const id = parseInt(searchText);
+          if (!isNaN(id)) {
+            query = { id };
+          }
+        } else if (searchText === 'text') {
+          query = {
+            text: {
+              [sequelize.Op.like]: `%${searchText}%`
+            }
+          };
+        }
+      
+        // Query the model based on the search criteria
+        template.findAll({ where: query })
+          .then((results) => {
+            // Handle the retrieved results and send a response
+            res.send(results);
+          })
+          .catch((error) => {
+            console.error('Error retrieving results', error);
+            res.status(500).send('Error retrieving results');
+          });
+      });
