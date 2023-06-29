45c942d6f176553ff9e83701b917217f15302390
DavidHCCNguyen <creativespecials@gmail.com>
2023-06-29 19:01:06 +0930

    current user DB, updates to code and adding js

--- public/assets/js/user-dashboard.js
+++ public/assets/js/user-dashboard.js
@@ -1,1 +1,28 @@
-// Functions for the user dashboard user-dashboard.handlebars
+$(document).ready(function() {
+    // View button click event
+    $('#view-template').click(function() {
+      const selectedTemplateIndex = $('#template-list .selected').index();
+      $('.factsheet').hide();
+      $('.factsheet').eq(selectedTemplateIndex).show();
+    });
+  
+    // Add Comment button click event
+    $('#add-comment').click(function() {
+      $('.comment-input').addClass('hidden');
+      const selectedFactsheet = $('.factsheet:visible');
+      const commentInput = selectedFactsheet.find('.comment-input');
+      commentInput.removeClass('hidden');
+      commentInput.focus();
+    });
+  
+    // Save Comment button click event
+    $('#save-comment').click(function() {
+      const selectedFactsheet = $('.factsheet:visible');
+      const commentInput = selectedFactsheet.find('.comment-input');
+      const comment = commentInput.val();
+      // Do something with the comment, such as sending it to the server
+      // ...
+      commentInput.val('').addClass('hidden');
+    });
+  });
+  
