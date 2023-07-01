c5056e9490cdd5c2c49b6db74acb80d3aaabd54f
DavidHCCNguyen <creativespecials@gmail.com>
2023-07-02 00:27:19 +0930

    Beta version of dashboard

--- public/assets/js/user-dashboard.js
+++ public/assets/js/user-dashboard.js
@@ -1,18 +1,35 @@
 // user-dashboard.js
 
-// Get all the factsheet list items
-const factsheetItems = document.querySelectorAll('#view-factsheets .border-2');
-
-// Add click event listener to each factsheet list item
-factsheetItems.forEach((item, index) => {
-  item.addEventListener('click', () => {
-    // Hide all factsheet content
-    const factsheetContent = document.querySelectorAll('#view-posts .border-2');
-    factsheetContent.forEach(content => {
-      content.style.display = 'none';
+// Function to show the first factsheet box
+function showFirstFactsheet() {
+    // Hide all other factsheet boxes
+    const factsheetBoxes = document.querySelectorAll('.user-factsheet');
+    factsheetBoxes.forEach((box) => {
+      box.style.display = 'none';
     });
-
-    // Show the selected factsheet content
-    factsheetContent[index].style.display = 'block';
-  });
-});
+  
+    // Show the first factsheet box
+    const firstFactsheetBox = document.querySelector('.user-factsheet[data-id="1"]');
+    firstFactsheetBox.style.display = 'block';
+  }
+  
+  // Function to show the second factsheet box
+  function showSecondFactsheet() {
+    // Hide all other factsheet boxes
+    const factsheetBoxes = document.querySelectorAll('.user-factsheet');
+    factsheetBoxes.forEach((box) => {
+      box.style.display = 'none';
+    });
+  
+    // Show the second factsheet box
+    const secondFactsheetBox = document.querySelector('.user-factsheet[data-id="2"]');
+    secondFactsheetBox.style.display = 'block';
+  }
+  
+  // Attach event listeners to the title elements
+  const firstTitle = document.querySelector('.text-xl.font-bold.mb-2'); // Replace with the actual selector for the first title element
+  const secondTitle = document.querySelector('.text-xl.font-bold.mb-2'); // Replace with the actual selector for the second title element
+  
+  firstTitle.addEventListener('click', showFirstFactsheet);
+  secondTitle.addEventListener('click', showSecondFactsheet);
+  
