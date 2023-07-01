4d199f39d5f786d4c684cc9a879bfad6a0734756
DavidHCCNguyen <creativespecials@gmail.com>
2023-07-01 17:17:23 +0930

    SO close to a working clicker

--- public/assets/js/user-dashboard.js
+++ public/assets/js/user-dashboard.js
@@ -1,41 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const factsheets = [
-    {
-      category_id: 1,
-      title: "Understanding Glaucoma",
-      description: "Overview of Glaucoma",
-      template: {
-        html: `<p># Glaucoma: The Silent Thief of Vision</p>
-               <p>This is the HTML content for the Understanding Glaucoma factsheet.</p>`,
-        administrator_id: 1,
-      },
-      comments: [
-        // Comments for the Understanding Glaucoma factsheet
-      ],
-    },
-    {
-      category_id: 2,
-      title: "Understanding Myopia (Shortsightedness)",
-      description: "An overview of myopia (shortsightedness) and its causes, symptoms, risks, and interventions.",
-      template: {
-        html: "<p>This is the HTML content for the Understanding Myopia factsheet.</p>",
-        administrator_id: 1,
-      },
-      comments: [
-        // Comments for the Understanding Myopia factsheet
-      ],
-    },
-  ];
+// user-dashboard.js
 
-  // Check if the "Understanding Glaucoma" factsheet exists and add a click event listener
-  const understandingGlaucomaFactsheet = factsheets.find(factsheet => factsheet.title === 'Understanding Glaucoma');
-  if (understandingGlaucomaFactsheet) {
-    const understandingGlaucomaElement = document.getElementById(`fact-${understandingGlaucomaFactsheet.category_id}`);
+// Get all the factsheet list items
+const factsheetItems = document.querySelectorAll('#view-factsheets .border-2');
 
-    understandingGlaucomaElement.addEventListener('click', function() {
-      // Uncomment the section by removing the 'hidden' class
-      const viewPostsSection = document.getElementById('view-posts');
-      viewPostsSection.classList.remove('hidden');
+// Add click event listener to each factsheet list item
+factsheetItems.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    // Hide all factsheet content
+    const factsheetContent = document.querySelectorAll('#view-posts .border-2');
+    factsheetContent.forEach(content => {
+      content.style.display = 'none';
     });
-  }
+
+    // Show the selected factsheet content
+    factsheetContent[index].style.display = 'block';
+  });
 });
