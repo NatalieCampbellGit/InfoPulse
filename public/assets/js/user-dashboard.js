271901cf146f1ab68b746a4d980b881d97a4dbcc
DavidHCCNguyen <creativespecials@gmail.com>
2023-07-02 12:30:18 +0930

    Dashboard and js

--- public/assets/js/user-dashboard.js
+++ public/assets/js/user-dashboard.js
@@ -1,17 +1,43 @@
-// JavaScript
-const factsheets = document.querySelectorAll('.user-factsheet');
-
-function showFactsheet(factsheetId) {
-  factsheets.forEach((factsheet) => {
-    const id = factsheet.getAttribute('data-id');
-    
-    if (id === factsheetId) {
-      factsheet.classList.remove('hidden');
-    } else {
-      factsheet.classList.add('hidden');
+// user-dashboard.js
+/* eslint-disable no-undef */
+// Function to fetch the content for a specific factsheet
+async function fetchFactsheetContent(factsheetId) {
+  try {
+    const response = await fetch(`/api/factsheets/${factsheetId}`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch factsheet content");
     }
+    const data = await response.json();
+    return data.content;
+  } catch (error) {
+    console.log(error);
+    // Handle error case here
+  }
+}
+
+// Function to handle the click event on factsheet titles
+function handleClick(factsheetId) {
+  fetchFactsheetContent(factsheetId)
+    .then((content) => {
+      // Render the content on the page
+      document.getElementById("factsheet-content").innerHTML = content;
+    })
+    .catch((error) => {
+      console.log(error);
+      // Handle error case here
+    });
+}
+
+// Function to initialize the user dashboard
+function initializeDashboard() {
+  const factsheetTitles = document.getElementsByClassName("factsheet-title");
+
+  // Attach click event listeners to factsheet titles
+  Array.from(factsheetTitles).forEach((title) => {
+    const factsheetId = title.dataset.factsheetId;
+    title.addEventListener("click", () => handleClick(factsheetId));
   });
 }
 
-// Example usage
-showFactsheet('2');
+// Call the initializeDashboard function when the page is loaded
+document.addEventListener("DOMContentLoaded", initializeDashboard);
