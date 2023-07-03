1e1938016a75a74d4b7fb29589776bd53f16bf08
DavidHCCNguyen <creativespecials@gmail.com>
2023-07-03 16:48:01 +0930

    Working clicks, took too long

---
+++ public/assets/js/user-dashboard.js
@@ -0,0 +1,18 @@
+function toggleFactsheet(factsheetId) {
+    var factsheet = document.getElementById('factsheet-' + factsheetId);
+    if (factsheet.classList.contains('hidden')) {
+      factsheet.classList.remove('hidden');
+    } else {
+      factsheet.classList.add('hidden');
+    }
+  }
+  
+  // Add click event to factsheet titles
+  var factsheetTitles = document.querySelectorAll('#view-factsheets .border-2');
+  factsheetTitles.forEach(function(title) {
+    title.addEventListener('click', function() {
+      var factsheetId = this.getAttribute('data-factsheet-id');
+      toggleFactsheet(factsheetId);
+    });
+  });
+  
